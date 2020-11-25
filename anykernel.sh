dbea9b58f37eeeeba4c9b3f8c9ab628bf90d44e2
attack11 <fernandobouchet@gmail.com>
2020-11-25 19:47:36 -0300

    anykernel: Check zip and kernel installed first

--- anykernel.sh
+++ anykernel.sh
@@ -32,7 +32,44 @@ ramdisk_compression=auto;
 ## AnyKernel install
 dump_boot;
 
-cat $home/dtb > $split_img/kernel_dtb;
+case "$ZIPFILE" in
+  *Hmp*)
+    ui_print " " "You are flashing OC for HMP kernel..."
+    zip_type=HMP
+    ;;
+  *Eas*)
+    ui_print " " "You are flashing OC for EAS kernel..."
+    zip_type=EAS
+    ;;
+esac
+
+ui_print " " "Checking installed kernel type..."
+
+kernel_name="$(uname -r)";
+case "$kernel_name" in
+  *Hmp*)
+    ui_print " " "You are using HMP kernel..."
+    kernel_type=HMP
+    if [ "$kernel_type" != "$zip_type" ]; then
+    ui_print " "; ui_print "You are trying to flash $zip_type zip over $kernel_type kernel, please use the correct zip!";
+    exit 1;
+    else
+    ui_print " " "Everything its correct! Flashing..."
+    cat $home/dtb > $split_img/kernel_dtb;
+    fi;
+    ;;
+  *Eas*)
+    ui_print " " "You are using EAS kernel..."
+    kernel_type=EAS
+    if [ "$kernel_type" != "$zip_type" ]; then
+    ui_print " "; ui_print "You are trying to flash $zip_type zip over $kernel_type kernel, please use the correct zip!";
+    exit 1;
+    else
+    ui_print " " "Everything its correct! Flashing..."
+    cat $home/dtb > $split_img/kernel_dtb;
+    fi;
+    ;;
+esac
 
 write_boot;
 ## end install
